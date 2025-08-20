ea597edf4e305b302f82eaf0b300c6a91b8a6879
Adamil Mateo <adamilmateo6@gmail.com>
2025-08-20 10:50:59 -0400

    Update app

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// Variable para los nombres posibles del amigo secreto
+let amigos = ["Karla", "Diego", "Yorsenis", "Pablo", "Sony", "Junior", "Federico", "Franci"];
+console.log(amigos.length);
+
+// Funcion para agregar amigos
+function agregarAmigo() {
+    const input = document.getElementById("amigo");
+    const amigo = input.value.trim();
+
+    if (amigo === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(amigo);
+    input.value = "";
+
+    mostrarAmigos();
+}
+
+function mostrarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos disponibles para sortear.");
+        return;
+    }
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = "Amigo sorteado: " + amigoSorteado;
+}
